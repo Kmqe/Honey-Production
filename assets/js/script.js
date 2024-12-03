2e8e952dc71a9cde1761a37a8110c1c02c6b6d37
Kmqe <7moodi115b2@gmail.com>
2024-12-03 09:32:22 +0300

    feat: - Complete services section - Add about and hero video sections - Add honey types section - Integrate Swiper.js for improved UI functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,65 @@ navClose.addEventListener("click", () => {
     navToggle.classList.remove("hide")
     navClose.classList.remove("show")
 })
+
+
+new Swiper('.card_container', {
+    loop: true,
+    speed: 1000,
+    lazy: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination-services',
+        clickable: true,
+        // dynamicBullets: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
+
+
+new Swiper('.cards_honey', {
+    loop: true,
+    speed: 1000,
+    lazy: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination-honey',
+        clickable: true,
+        // dynamicBullets: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 2
+        }
+    }
+});
